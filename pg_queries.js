5f8f8157dea061145760cafcee6a7441334876d4
Cardoran <ccardoran@gmail.com>
2026-01-27 12:36:01 +0100

    return on conflict

--- pg_queries.js
+++ pg_queries.js
@@ -206,8 +206,8 @@ export class pg_query_handler {
                 `INSERT INTO stations (station_name)
                  VALUES ($1)
                  ON CONFLICT (station_name)
-                 DO UPDATE SET station_name = EXCLUDED
-                 RETURNING old.id`,
+                 DO RETURNING old.id
+                 RETURNING id`,
                 [tt_data.timetable.station]
             );
             const station_id = station_result.rows[0].id;
