fd97ea26da8ebacd9adb6bdf8758c00fe835fada
Cardoran <ccardoran@gmail.com>
2026-01-27 16:23:36 +0100

    add station as filter

--- pg_queries.js
+++ pg_queries.js
@@ -221,9 +221,9 @@ export class pg_query_handler {
                 )
                 SELECT id FROM res
                     UNION ALL
-                SELECT id FROM stations WHERE station_name=$1
+                SELECT id FROM stations WHERE station_name=$3
                 LIMIT 1`,
-                [keyslist, valueslist]
+                [keyslist, valueslist, tt_data.timetable.station]
             );
             const station_id = station_result.rows[0].id;
             console.log("stored data to:", station_id);
