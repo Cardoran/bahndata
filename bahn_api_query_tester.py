9029dbf3da432646acd9a121051b4bb126927983
Cardoran <ccardoran@gmail.com>
2026-01-27 10:23:51 +0100

    python script to retreive and view query results

---
+++ bahn_api_query_tester.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+import requests
+import xmltodict
+
+def get_timetable_xml(api_auth, EVA_NR, date:datetime = None) -> str:
+    if date is None:
+        date = datetime.now()
+    date_string = date.strftime("%y%m%d")
+    hour: str = date.strftime("%H")
+    url = f"https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/{EVA_NR}/{date_string}/{hour}"
+    # print(url)
+    response = requests.get(
+        f"https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1"
+        f"/plan/{EVA_NR}/{date_string}/{hour}",
+        headers=api_auth
+    )
+    print(response.url)
+
+    # print("'{}'".format(response.text))
+    # with open("plan.txt", "w") as text_file:
+    #     text = response.text
+    #     text = text.replace("><",">\n<")
+    #     text_file.write(text)
+
+    if response.status_code == 410:
+        return get_timetable_xml(api_auth,EVA_NR,date+timedelta(days=1))
+    elif response.status_code == 401:
+        raise Exception("Code 401: Can't request timetable because the credentials are not correct. Please make sure that "
+                        "you are providing the correct credentials.")
+    elif response.status_code == 400:
+        raise Exception("Code 400: Can't request timetable because the EVA number is not correct. Please make sure that "
+                        "you are providing the correct EVA number.")
+    elif response.status_code != 200:
+        raise Exception("Can't request timetable! The request failed with the HTTP status code {}: {}"
+                        .format(response.status_code, response.text))
+    return response.text
+
+if __name__ == "__main__":
+    api_auth = {
+                "DB-Api-Key": "235a6da868e721b3ed0f8915d17759fb",
+                "DB-Client-Id": "2b83a09f021fad54d68cc31e3b5e03e2",
+            }
+    xml = get_timetable_xml(api_auth, 8000068)
+    datadict = xmltodict.parse(xml)["timetable"]
+    print(datadict.keys())
+    print(datadict["@station"])
+    for s in datadict["s"]:
+        print(s)
