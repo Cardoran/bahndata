52db41ff583e98c5ae1632488da78ba0f30859bc
Cardoran <ccardoran@gmail.com>
2026-01-28 19:50:24 +0100

    await for clear order

--- pg_queries.js
+++ pg_queries.js
@@ -295,7 +295,7 @@ export class pg_query_handler {
                 if (data[key] == '') {
                     continue;
                 }
-                this.insert_data(key, data[key], table_key, row_id);
+                await this.insert_data(key, data[key], table_key, row_id);
             }
         }
         catch (error) {
