7ae73d34f29090103bb70bd2aa9eca35b53d8205
Cardoran <ccardoran@gmail.com>
2026-03-06 14:31:19 +0100

    remove the json assertion

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 import express from 'express';
 import { miner_coordinator } from './miner.js';
 import { pg_query_handler } from './pg_queries.js';
-import apiHeaders from './.auth' assert { type: 'json' };
+import apiHeaders from './.auth';
 
 const app = express();
 const port = 80;
